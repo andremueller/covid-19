484ec59f92ca7b1466ac3394dec6dfe0d02a6ce5
Andre Mueller <andre@kiwisound.de>
2020-03-22 12:12:02 +0100

    Summary of world rate

--- R/covid.R
+++ R/covid.R
@@ -27,12 +27,45 @@ y <- read_csv("../COVID-19/data/cases_time.csv",
                 Delta_Confirmed = col_double(),
                 Delta_Recovered = col_double()
               )) %>% mutate(Last_Update = mdy(Last_Update))
+events <- read_csv("events.csv") %>% mutate(Date = ymd(Date))
 
-y1 <- y %>% 
+world_time <- y %>% group_by(Last_Update) %>% 
+  summarize(Confirmed = sum(Confirmed), 
+            Deaths = sum(Deaths),
+            Recovered = sum(Recovered),
+            Active = sum(Active),
+            Mortality_Rate = Deaths/Confirmed * 100.0) %>%
+  ungroup() %>%
+  pivot_longer(-Last_Update)
+
+p <- world_time %>% ggplot(aes(Last_Update, y=value, color=name)) +
+  theme_bw() + geom_line() + ggtitle('Worldwide COVID-19')
+#scale_x_log10() + annotation_logticks()
+print(plotly::ggplotly(p, dynamicTicks = TRUE))
+
+LOWER_LIMIT <- 10
+y1 <- y %>%
   group_by(Country_Region) %>%
   mutate(Replication_Rate = (Confirmed - lag(Confirmed)) / lag(Confirmed)) %>%
+  filter(Confirmed >= LOWER_LIMIT) %>%
+  mutate(Day = row_number()) %>%
   ungroup()
+
+Ev <- left_join(events, y1 %>% filter(Day==1)) %>% select(-Day) %>%
+  rename(Outbreak = Last_Update) %>%
+  mutate(Day = time_length(Date - Outbreak, 'days'))
+
+y_current <- y1 %>% group_by(Country_Region) %>% slice(n()) %>% 
+  ungroup() %>% arrange(desc(Confirmed)) %>% 
+  mutate(Confirmed_per_Day = Confirmed / Day)
+View(y_current)
+
+total <- y %>% group_by(Country_Region) %>% slice(n()) %>% ungroup() %>%
+  summarize(max(Last_Update), sum(Confirmed), sum(Deaths), sum(Recovered))
+View(total)
+
 sel <- c('Germany', 'China', 'Italy', 'France')
+sel <- y_current$Country_Region[1:8]
 p <- y1 %>% 
   filter(Country_Region %in% sel) %>%
   ggplot(aes(Last_Update, Replication_Rate, text = Confirmed, color = Country_Region)) + geom_line()
@@ -40,3 +73,23 @@ q <- plotly::ggplotly(p, dynamicTicks = TRUE)
 print(q)
 View(y %>% filter(Country_Region=='Germany'))
 
+y2 <- y1 %>% filter(Country_Region %in% sel) %>% 
+  mutate(info = paste0(Last_Update, " Confirmed = ", Confirmed))
+
+# Active
+p1 <- y2 %>%
+  ggplot(aes(Day, Active, color = Country_Region, text = Confirmed)) + 
+  theme_bw() +
+  geom_line() + geom_line(aes(y=Deaths), linetype='dotted')  #+ scale_y_log10()
+print(p1)
+q1 <- plotly::ggplotly(p1, dynamicTicks = TRUE)
+print(q1)
+
+# Confirmed
+p1 <- y2 %>%
+  ggplot(aes(Day, Confirmed, color = Country_Region)) + 
+  theme_bw() + 
+  geom_line() + geom_line(aes(y=Deaths), linetype='dotted')  #+ scale_y_log10()
+print(p1)
+q1 <- plotly::ggplotly(p1, dynamicTicks = FALSE)
+print(q1)
---
+++ README.md
@@ -0,0 +1,13 @@
+# COVID-19 Analysis
+
+https://www.visualcapitalist.com/infection-trajectory-flattening-the-covid19-curve/
+  
+## Measures
+
+- Quarantining
+- Encouraging social distancing
+- Encouraging working from home
+- Closing schools and other institutions
+- Placing hard limits on the size of crowds at events
+- Lockdown
+- Travel Bans
